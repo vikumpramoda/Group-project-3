9030b32b0e1eaa9862bb1164899f309ca16df8c1
Tharindu-MIT020 <59837933+Tharindu-MIT020@users.noreply.github.com>
2021-07-09 19:54:01 +0530

    on folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-
 import './App.css';
 import {BrowserRouter as  Router,Switch, Route} from 'react-router-dom'
-import Test_Homepage from './Test_Homepage';
+import Test_Homepage from './Pages/Test_Homepage';
 
 function App() {
   return (
---
+++ src/Pages/Test_Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function Test_Homepage() {
+    
+    return (
+        <div>
+            <h1>
+                Welcome to Group project 3
+            </h1>
+        </div>
+    )
+}
+
+export default Test_Homepage;
