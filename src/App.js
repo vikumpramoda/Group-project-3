f4c04bdb58874fa8342af26c6c36311ffd4f3f86
Tharindu-MIT020 <59837933+Tharindu-MIT020@users.noreply.github.com>
2021-07-09 20:34:59 +0530

    bropdoun update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {BrowserRouter as  Router,Switch, Route} from 'react-router-dom'
 import Test_Homepage from './Pages/Test_Homepage';
+import DropDown from './Pages/DropDownlist';
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
 
       <Switch>
 
-        <Route path= '/' component = {Test_Homepage}/>
+       
+          <Route path='/' component={DropDown}/>
+
 
       </Switch>
 
