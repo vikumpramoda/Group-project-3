a11d88f9e3cef0e13a6ebc5b8788dbf294f2e657
vikum <pramodavikum@gmail.com>
2021-07-09 21:14:06 +0530

    chart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as  Router,Switch, Route} from 'react-router-dom'
 import Test_Homepage from './Pages/Test_Homepage';
 import DropDown from './Pages/DropDownlist';
 import Chart from './Pages/chart';
+import ChartsNavbBar from './Pages/chartNavBar';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
        
           <Route path='/' component={DropDown}/>
           <Route path='/chart' component={Chart}/>
+          <Route path='/nav' component={ChartsNavbBar}/>
 
 
       </Switch>
